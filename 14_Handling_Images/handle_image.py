7171d3bece32be51ab9c32dcceb25837a7a6e67e
fhblf <fhblf@tutanota.com>
2024-12-09 20:19:24 +0100

    modified:   14_Handling_Images/handle_image.py

--- 14_Handling_Images/handle_image.py
+++ 14_Handling_Images/handle_image.py
@@ -1,18 +1,18 @@
 from decouple import config
-from langchain_mistralai import ChatMistralAI
+from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.prompts import ChatPromptTemplate
 
 import base64
 
-MISTRALAI_KEY = config("MISTRALAI_KEY")
+GOOGLE_GEMINI_KEY = config("GOOGLE_GEMINI_KEY")
 
-llm = ChatMistralAI(model="mistral-large-latest", api_key=MISTRALAI_KEY)
+llm = ChatGoogleGenerativeAI(model="gemini-1.5-pro-latest", google_api_key=GOOGLE_GEMINI_KEY)
 
 def encode_image(image_path):
     with open(image_path, "rb") as image_file:
         return base64.b64encode(image_file.read()).decode("utf-8")
 
-image = encode_image("./NW-Doge.jpg")
+image = encode_image("14_Handling_Images/NW-Doge.jpg")
 
 prompt = ChatPromptTemplate.from_messages(
     [
